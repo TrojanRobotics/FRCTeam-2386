bfbde0a213dbcefa6eff409a24b04fdad92259b7
Laxman Sooriyathas <laxman0202@gmail.com>
2012-02-04 11:04:46 -0500

    Added a method called setToAngle This method gets the starting angle of the robot and then moves the robot to the new angle.

--- Year2012/com/BCHS/BCHSLib.java
+++ Year2012/com/BCHS/BCHSLib.java
@@ -1,12 +1,29 @@
 package Year2012.com.BCHS;
 
 import edu.wpi.first.wpilibj.Gyro;
+import edu.wpi.first.wpilibj.RobotDrive;
 
 
 public class BCHSLib
 {
-    public static void setToAngle(Gyro gyroscope, double newAngle)
+    public void setToAngle(RobotDrive driveTrain, Gyro gyroscope, double newAngle)
 	{
-		
+		double startAngle = gyroscope.getAngle();
+		if (newAngle > 0)
+		{
+			while (gyroscope.getAngle() != startAngle + newAngle)
+			{
+				driveTrain.drive(0.5, 1.0);
+			}
+			driveTrain.stopMotor();
+		}
+		else if (newAngle < 0) 
+		{
+			while (gyroscope.getAngle() != startAngle - newAngle)
+			{
+				driveTrain.drive(0.5, -1.0);
+			}
+			driveTrain.stopMotor();
+		}
 	}
 }

1b7f4a7e00e28193802e951bd05e1cb2477c3c1e
Laxman Sooriyathas <laxman0202@gmail.com>
2012-02-09 15:33:46 -0500

    Updated SetToAngle and added a new function changed SetToAngle to use range and it now works. added ultrasonic range converter

--- Year2012/com/BCHS/BCHSLib.java
+++ Year2012/com/BCHS/BCHSLib.java
@@ -1,29 +1,40 @@
 package Year2012.com.BCHS;
 
+import edu.wpi.first.wpilibj.AnalogChannel;
 import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.RobotDrive;
+
 
 
 public class BCHSLib
 {
     public void setToAngle(RobotDrive driveTrain, Gyro gyroscope, double newAngle)
-	{
+	{		
 		double startAngle = gyroscope.getAngle();
-		if (newAngle > 0)
+		
+		if (newAngle > 0)	
 		{
-			while (gyroscope.getAngle() != startAngle + newAngle)
+			while ((int)gyroscope.getAngle() <= (int)(startAngle + newAngle - 1) || (int)gyroscope.getAngle() >= (int)(startAngle + newAngle + 1))
 			{
-				driveTrain.drive(0.5, 1.0);
+				driveTrain.drive(0.3, 1.0);
+				System.out.println("Moving 1");
 			}
-			driveTrain.stopMotor();
+			System.out.println("Stopped 1");
 		}
 		else if (newAngle < 0) 
 		{
-			while (gyroscope.getAngle() != startAngle - newAngle)
+			while ((int)gyroscope.getAngle() <= (int)(startAngle - newAngle - 1) || (int)gyroscope.getAngle() >= (int)(startAngle - newAngle + 1))
 			{
-				driveTrain.drive(0.5, -1.0);
+				driveTrain.drive(0.3, -1.0);
+				System.out.println("Moving");
 			}
-			driveTrain.stopMotor();
+			System.out.println("Stopped");
 		}
+		
+		driveTrain.stopMotor();
+	}
+	public double Ultrasonicdistance(AnalogChannel Ultrasonic)
+	{
+		return (Ultrasonic.getVoltage()*1000.0/9.766);
 	}
 }
